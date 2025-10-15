77a8a39eca5251607637f789cdb9b67db1620656
Miguel A. Olmedo <miguelangelolmedo24@outlook.es>
2025-10-15 16:41:30 -0600

    Add files via upload

---
+++ Ejercicio 3/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+       
+       Template JDK = new Template();
+        
+       
+        JDK.mostrarjdk();
+    }
+}
---
+++ Ejercicio 3/Template.java
@@ -0,0 +1,13 @@
+class Template {
+    
+    public String mostrarjdk() {
+        String resultado = "*\n" +
+                          "J: Java\n" +
+                          "D: Development\n" +
+                          "K: Kit\n" +
+                          "*";
+        
+        System.out.println(resultado);
+        return resultado;
+    }
+}
