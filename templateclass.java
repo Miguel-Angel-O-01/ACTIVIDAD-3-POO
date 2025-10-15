25ee1103be4865752079e13a7ac207019104c5b1
Miguel Angel Cruz Olmedo <miguelangelolmedo24@outlook.es>
2025-10-15 13:55:03 -0600

    first commit

---
+++ templateclass.java
@@ -0,0 +1,11 @@
+public class templateclass {
+	// Variables para una reservación del restaurante
+	public String nombreCliente;  
+	public String apellidoCliente;
+	public String direccionCliente;
+	public String fecha;           
+	public String hora;            
+	public int numeroPersonas;     
+	public String telefonoContacto; 
+
+}
